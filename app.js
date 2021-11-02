9a414e9a4a5f33d426e3820e9f3787eef4b8f73e
Kunwar Pratap <kunwarpratap353@gmail.com>
2021-11-02 21:19:52 +0530

    input output processing done

--- app.js
+++ app.js
@@ -3,12 +3,13 @@
 // alert("this is the usrname "+ username)
 //
 var btnTranslate = document.querySelector("#btn-translate");  
-var txtInput = document.querySelector("#txt-input") 
-console.log(txtInput)
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output");
+
+////console.log(txtInput)
 // reference to the button
 // console.log(btnTranslate);
 function clickHandler(){
-   console.log("clicked");
-   console.log("input",txtInput.value);
+   outputDiv.innerText = "aknfnanavn  "+ txtInput.value;
 };
 btnTranslate.addEventListener("click", clickHandler);
