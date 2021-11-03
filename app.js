86ad40f8310a9ee32bd8f185ea2b6dea9671b539
Kunwar Pratap <kunwarpratap353@gmail.com>
2021-11-04 01:48:12 +0530

    added few more lines

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //
 var btnTranslate = document.querySelector("#btn-translate");  
 var txtInput = document.querySelector("#txt-input")
-var outputDiv = document.querySelector("#output");
+var outputDiv = document.querySelector("#translate-output");
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverURL = 	"https://api.funtranslations.com/translate/minion.json"
 
@@ -24,7 +24,7 @@ function errorHandler(error){
 function clickHandler(){
 
   var inputText = txtInput.value; //input
-  
+
   // calling server for processing
   fetch(getTranslationURL(inputText))
   .then(Response => Response.json())
