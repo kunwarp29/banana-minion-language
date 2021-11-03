52fe2bae6731d722d7609a37d48637cabb7b588a
Kunwar Pratap <kunwarpratap353@gmail.com>
2021-11-04 01:09:29 +0530

    done

--- app.js
+++ app.js
@@ -5,11 +5,34 @@
 var btnTranslate = document.querySelector("#btn-translate");  
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = 	"https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+   return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+   console.log("error occured",error)
+   alert("something wrong with server! try again after some time")
+}
 
 ////console.log(txtInput)
 // reference to the button
 // console.log(btnTranslate);
+
 function clickHandler(){
-   outputDiv.innerText = "aknfnanavn  "+ txtInput.value;
+
+  var inputText = txtInput.value; //input
+  
+  // calling server for processing
+  fetch(getTranslationURL(inputText))
+  .then(Response => Response.json())
+  .then(json => {
+     var translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText; //output
+
+  }
+  ).catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler);
