b6631b4f96884cc1249d21a7d0958653949559a9
kunwarp29 <kunwarpratap353@gmail.com>
2024-05-28 00:21:24 +0530

    added few changes

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// // console.log("hi its me")
-// var username = prompt("give me usename")
-// alert("this is the usrname "+ username)
-//
+
 var btnTranslate = document.querySelector("#btn-translate");  
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#translate-output");
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverURL = 	"https://api.funtranslations.com/translate/minion.json"
 
 
@@ -17,9 +13,6 @@ function errorHandler(error){
    alert("something wrong with server! try again after some time")
 }
 
-////console.log(txtInput)
-// reference to the button
-// console.log(btnTranslate);
 
 function clickHandler(){
 
